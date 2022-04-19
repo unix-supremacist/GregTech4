f342c5cda07fb9f57a2f5e998af3986656c410ee
DrParadox7 <47131096+DrParadox7@users.noreply.github.com>
2022-04-19 20:37:15 +0100

    Pneumatic Upgrade takes Tungstensteel Gear
    instead of Steel.

--- src/main/java/gregtechmod/loaders/oreprocessing/ProcessingCrafting.java
+++ src/main/java/gregtechmod/loaders/oreprocessing/ProcessingCrafting.java
@@ -39,8 +39,7 @@ public class ProcessingCrafting implements IOreRecipeRegistrator {
 					List<ItemStack> filtered = entry.ores.stream().filter(s -> !s.isItemEqual(machineBlock)).collect(Collectors.toList());
 					RecipeMaps.ASSEMBLING.factory().EUt(2).duration(1600).input(RecipeEntry.fromStacks(filtered, Match.DAMAGE)).input(GregTech_API.getGregTechComponent(22, 1)).output(new ItemStack(GregTech_API.sBlockList[1], 1, 79)).buildAndRegister();
 				} else if (entry.oreDictName.equals(GT_OreDictNames.craftingGenerator.toString())) {
-					RecipeMaps.ASSEMBLING.factory().EUt(4).duration(3200).input(RecipeEntry.fromStacks(entry.ores, Match.DAMAGE)).input(OrePrefixes.gearGt, Materials.Steel, 1).output(GregTech_API.getGregTechComponent(25, 1)).buildAndRegister();
-					RecipeMaps.ASSEMBLING.factory().EUt(4).duration(3200).input(RecipeEntry.fromStacks(entry.ores, Match.DAMAGE)).input(OrePrefixes.gearGt, Materials.StainlessSteel, 1).output(GregTech_API.getGregTechComponent(25, 1)).buildAndRegister();
+					RecipeMaps.ASSEMBLING.factory().EUt(4).duration(3200).input(RecipeEntry.fromStacks(entry.ores, Match.DAMAGE)).input(OrePrefixes.gearGt, Materials.TungstenSteel, 1).output(GregTech_API.getGregTechComponent(25, 1)).buildAndRegister();
 				} else if (entry.oreDictName.equals(GT_OreDictNames.craftingWireCopper.toString())) {
 					RecipeMaps.ASSEMBLING.factory().EUt(2).duration(1600).input(RecipeEntry.fromStacks(2, entry.ores, Match.DAMAGE)).input(OrePrefixes.plate, Materials.BatteryAlloy, 6).output(GT_Items.Battery_Hull_MV.get(1)).buildAndRegister();
 					RecipeMaps.ASSEMBLING.factory().EUt(1).duration(800).input(RecipeEntry.fromStacks(3, entry.ores, Match.DAMAGE)).input(GT_Items.Circuit_Board_Basic.get(1)).output(GT_Items.Circuit_Basic.get(1)).buildAndRegister();
